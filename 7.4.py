b0b924c50dca04b206aeee5e1feda76b8318e106
Becky Rieger <brieger@ivytech.edu>
2024-11-07 15:26:28 -0600

    capitalize person

--- 7.4.py
+++ 7.4.py
@@ -1,2 +1,5 @@
 #make a list called things with these three strings as elements: 'mozzarella', 'cinderella','salmonella'
 things = ['mozzarella', 'cinderella', 'salmonella']
+#capitalize the element in things that refers to a person, then print
+things[1] = things[1].capitalize()
+print(things)
