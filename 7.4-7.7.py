58443ecafa5ec08e05a755d2763c1cf2c7a7ad0e
Becky Rieger <brieger@ivytech.edu>
2024-11-07 15:33:17 -0600

    cheesy uppercase

--- 7.4-7.7.py
+++ 7.4-7.7.py
@@ -3,3 +3,6 @@ things = ['mozzarella', 'cinderella', 'salmonella']
 #capitalize the element in things that refers to a person, then print
 things[1] = things[1].capitalize()
 print(things)
+#make the cheesy element of things all uppercase and print list
+things[0]= things[0].upper()
+print(things)
